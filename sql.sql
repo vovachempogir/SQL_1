53e9b033dc3b9f66ded4fbdd683bee62bec05c40
Vladimir <vovachempogir@mail.ru>
2023-05-12 18:14:02 +0700

    3

--- sql.sql
+++ sql.sql
@@ -1,5 +1,11 @@
+    Домашнее задание 1
+        Задание 1
+
     CREATE DATABASE skypro;
     \c skypro
+
+        Задание 2
+
     CREATE TABLE employee(
         id BIGSERIAL NOT NULL PRIMARY KEY,
         first_name VARCHAR(50) NOT NULL,
@@ -7,6 +13,8 @@
         gender VARCHAR(6) NOT NULL
         age INT NOT NULL
     );
+
+        Задание 3
 
     INSERT INTO employee(
         first_name, last_name, gender, age
@@ -36,8 +44,8 @@
 
     SELECT * FROM employee;
 ___________________________________________________________________________________
-
-// Задание 1
+    Домашнее задание 2
+        Задание 1
 
     INSERT INTO employee(
         first_name, last_name, gender, age
@@ -75,7 +83,7 @@ ________________________________________________________________________________
     From employee
     where first_name like '_____%';
 
-//Задание 2
+        Задание 2
 
     UPDATE employee
     SET first_name='Владимир',
@@ -110,3 +118,74 @@ ________________________________________________________________________________
                FROM employee
                WHERE first_name = 'Светлана')
     ORDER BY age;
+___________________________________________________________________________________
+    Домашнее задание 3
+        Задание 1
+
+    CREATE TABLE city(
+        city_id BIGSERIAL NOT NULL PRIMARY KEY,
+        city_name VARCHAR(50) NOT NULL);
+
+    ALTER TABLE employee
+        ADD city_id INT;
+
+    ALTER TABLE employee
+        ADD CONSTRAINT city_id
+        FOREIGN KEY (city_id) REFERENCES city(city_id);
+
+    INSERT INTO city(city_name)
+    VALUES ('Санкт-Петербург');
+
+    INSERT INTO city(city_name)
+    VALUES ('Москва');
+
+    INSERT INTO city(city_name)
+    VALUES ('Дудинка');
+
+    INSERT INTO city(city_name)
+    VALUES ('Красноярск');
+
+    Update employee
+        SET city_id = '1'
+        WHERE first_name = 'Владимир';
+
+    Update employee
+        SET city_id = '2'
+        WHERE first_name = 'Светлана';
+
+    UPDATE employee
+        SET city_id = '3'
+        WHERE last_name = 'Сырчин';
+
+        Задание 2
+
+    SELECT employee.first_name AS Имя,
+           employee.last_name  AS фамилия,
+           city.city_name      AS Город
+    FROM employee
+             INNER JOIN city
+                        ON employee.city_id = city.city_id;
+
+    SELECT employee.first_name AS Имя,
+           employee.last_name  AS фамилия,
+           city.city_name      AS Город
+    FROM employee
+             RIGHT JOIN city
+                        ON employee.city_id = city.city_id;
+
+    INSERT INTO employee(first_name, last_name, gender, age)
+    VALUES ('Юлия', 'Кат', 'жен', 32);
+
+    SELECT employee.first_name AS Имя,
+           employee.last_name  AS фамилия,
+           city.city_name      AS Город
+    FROM employee
+             FULL JOIN city
+                       ON employee.city_id = city.city_id;
+
+    SELECT employee.first_name AS Имя,
+           employee.last_name  AS фамилия,
+           city.city_name      AS Город
+    FROM employee
+             CROSS JOIN city;
+___________________________________________________________________________________
